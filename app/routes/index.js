76db84e468fa54fd13d180b27259566483ef81bd
onnia <kualtonen@gmail.com>
2014-10-22 00:15:21 +0300

    Putting the index.html content to jade file and moving the style and scripts files to public dir

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' })
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
