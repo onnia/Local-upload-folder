745e0da4471fbb4503f4171dd30efb01e9e5fb41
onnia <kualtonen@gmail.com>
2014-11-24 21:19:30 +0200

    Change the root folder to app folder

---
+++ tests/instrument/tests/app_spec.js
@@ -0,0 +1,9 @@
+
+var __cov_pdKAGnxOg4DvGNCSO0ILFg = (Function('return this'))();
+if (!__cov_pdKAGnxOg4DvGNCSO0ILFg.__coverage__) { __cov_pdKAGnxOg4DvGNCSO0ILFg.__coverage__ = {}; }
+__cov_pdKAGnxOg4DvGNCSO0ILFg = __cov_pdKAGnxOg4DvGNCSO0ILFg.__coverage__;
+if (!(__cov_pdKAGnxOg4DvGNCSO0ILFg['tests/app_spec.js'])) {
+   __cov_pdKAGnxOg4DvGNCSO0ILFg['tests/app_spec.js'] = {"path":"tests/app_spec.js","s":{"1":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":2,"column":0},"end":{"line":2,"column":31}}},"branchMap":{}};
+}
+__cov_pdKAGnxOg4DvGNCSO0ILFg = __cov_pdKAGnxOg4DvGNCSO0ILFg['tests/app_spec.js'];
+__cov_pdKAGnxOg4DvGNCSO0ILFg.s['1']++;var app=require('../app.js');
---
+++ tests/instrument/tests/help_spec.js
@@ -0,0 +1,9 @@
+
+var __cov_MoHDFrcUQ644yeFwXTUgsw = (Function('return this'))();
+if (!__cov_MoHDFrcUQ644yeFwXTUgsw.__coverage__) { __cov_MoHDFrcUQ644yeFwXTUgsw.__coverage__ = {}; }
+__cov_MoHDFrcUQ644yeFwXTUgsw = __cov_MoHDFrcUQ644yeFwXTUgsw.__coverage__;
+if (!(__cov_MoHDFrcUQ644yeFwXTUgsw['tests/help_spec.js'])) {
+   __cov_MoHDFrcUQ644yeFwXTUgsw['tests/help_spec.js'] = {"path":"tests/help_spec.js","s":{"1":0,"2":0,"3":0,"4":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":6,"loc":{"start":{"line":6,"column":16},"end":{"line":6,"column":30}}}},"statementMap":{"1":{"start":{"line":2,"column":0},"end":{"line":2,"column":36}},"2":{"start":{"line":6,"column":0},"end":{"line":11,"column":2}},"3":{"start":{"line":7,"column":4},"end":{"line":7,"column":19}},"4":{"start":{"line":10,"column":4},"end":{"line":10,"column":16}}},"branchMap":{}};
+}
+__cov_MoHDFrcUQ644yeFwXTUgsw = __cov_MoHDFrcUQ644yeFwXTUgsw['tests/help_spec.js'];
+__cov_MoHDFrcUQ644yeFwXTUgsw.s['1']++;var app=require('../lib/help.js');__cov_MoHDFrcUQ644yeFwXTUgsw.s['2']++;exports.pjson=function(test){__cov_MoHDFrcUQ644yeFwXTUgsw.f['1']++;__cov_MoHDFrcUQ644yeFwXTUgsw.s['3']++;test.expect(1);__cov_MoHDFrcUQ644yeFwXTUgsw.s['4']++;test.done();};
