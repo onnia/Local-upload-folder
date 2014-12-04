e543c7dd71949956e52b075be727bdb2be13ad22
onnia <kualtonen@gmail.com>
2014-12-04 23:27:53 +0200

    Now grunt is watching for changes in 'upload folder'

--- gruntfile.js
+++ gruntfile.js
@@ -3,15 +3,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({      
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['../upload/*.jpg', ' ../upload/*.png', ' ../upload/*.JPG', ' ../upload/*.jpeg'],
-        dest: '/public/img/'
-      }
-    },
     env: {
       coverage: {
         APP_DIR_FOR_CODE_COVERAGE: '../tests/coverage/instrument/app/'
@@ -37,35 +28,66 @@ module.exports = function(grunt) {
         print: 'detail'
       }
     },
+    /* Defines all files to be tested*/
     jshint: {
        all: ['Gruntfile.js', 'app.js', 'tests/*.js', 'lib/*.js', 'routes/*.js']
     },
     qunit: {
        files: ['tests/*.js']
     },
+    sync: {
+      main: {
+        files: [{
+          cwd: 'upload',
+          src: [
+            '**' /* Include everything */
+          ],
+          dest: 'public/img/'
+        }],
+        tasks: ['watch'],
+        pretend: true, // Don't do any IO. Before you run the task with `updateAndDelete` PLEASE MAKE SURE it doesn't remove too much.
+        verbose: true, // Display log messages when copying files
+        updateAndDelete: true // Remove all files from dest that are not found in src
+        }
+    },
+    /* Files to be copied */
+    copy: {
+        main: {
+            src: 'upload/*.jpg',
+            dest: 'public/img/'
+        }
+    },
     watch: {
-      files: ['../upload/*.jpg'],
-      tasks: ['concat', 'qunit']
+        scripts: {
+            /* Folder to watch */    
+            files: ['upload/*'],
+            tasks: ['sync:main', 'copy:main'],
+            options: {
+                spawn: false,
+                reload: true
+            }
+        }
     }
   });
 
-// grunt.loadNpmTasks('grunt-contrigru b-jshint');
 grunt.loadNpmTasks('grunt-env');
+grunt.loadNpmTasks('grunt-sync');
 grunt.loadNpmTasks('grunt-istanbul');
+grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-qunit');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 
-// Register tasks use them with grunt 'taskname'
+// Registered tasks grunt tasks
 grunt.registerTask('hint', ['jshint']);
-
+grunt.registerTask('files', ['sync:main']);
 grunt.registerTask('test', ['qunit', 'jshint'], function() {
   grunt.log.write('Testing and hinting...');   
 });
-
-grunt.registerTask('default', ['concat'], function() {
-  grunt.log.write('Logging some stuff...').ok();   
+  
+grunt.registerTask('default', ['watch', 'sync:main'], function() {
+   grunt.log.write('Watching upload folder for file changes...');   
 });
 
 grunt.registerTask('cover', ['env:coverage','instrument', 'storeCoverage', 'makeReport']);
