03453fccefe7b6b5d6dad378925e02f417627894
onnia <kualtonen@gmail.com>
2014-12-14 15:47:38 +0200

    Adding coveralls and grunt parallal task for connect server and watch task

--- gruntfile.js
+++ gruntfile.js
@@ -33,6 +33,22 @@ module.exports = function(grunt) {
             command: 'node ./bin/www'
         }
     },
+    /* code coveridge copied from docs*/
+    coveralls: {
+    options: {
+      // LCOV coverage file relevant to every target
+      src: 'coverage-results/lcov.info',
+
+      // When true, grunt-coveralls will only print a warning rather than
+      // an error, to prevent CI builds from failing unnecessarily (e.g. if
+      // coveralls.io is down). Optional, defaults to false.
+      force: false
+    },
+    your_target: {
+      // Target-specific LCOV coverage file
+      src: 'coverage-results/extra-results-*.info'
+    },
+  },
     /* Defines all files to be tested*/
     jshint: {
        all: ['Gruntfile.js', 'app.js', 'tests/*.js', 'lib/*.js', 'routes/*.js']
@@ -62,15 +78,35 @@ module.exports = function(grunt) {
             dest: 'public/img/'
         }
     },
+    taskinterval: {
+    dist: {
+        options: {
+            taskIntervals: [
+            {
+                interval: 1000 * 3, 
+                tasks: ['shell:start', 'jshint']}
+            ]
+        }
+    }
+  },
     watch: {
         scripts: {
             /* Folder to watch */    
             files: ['upload/*'],
-            tasks: ['sync:main', 'copy:main', 'shell:start'],
+            tasks: ['sync:main', 'copy:main'],
             options: {
                 spawn: false,
                 reload: true
             }
+        }
+    },
+    /* This task runs message connect and file syncs at the same time */
+    parallel: {
+    assets: {
+      options: {
+        grunt: true
+      },
+      tasks: ['alldone', 'shell:start', 'watch']
         }
     }
   });
@@ -78,19 +114,29 @@ module.exports = function(grunt) {
 grunt.loadNpmTasks('grunt-env');
 grunt.loadNpmTasks('grunt-sync');
 grunt.loadNpmTasks('grunt-shell');
+grunt.loadNpmTasks('grunt-parallel');
 grunt.loadNpmTasks('grunt-istanbul');
 grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-task-interval');
 grunt.loadNpmTasks('grunt-contrib-qunit');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-coveralls');
 
 // Registered tasks grunt tasks
+
+grunt.registerTask('inte', ['taskinterval']);
 grunt.registerTask('hint', ['jshint']);
-grunt.registerTask('default', ['shell:start']);
+grunt.registerTask('default', ['alldone', 'watch', 'shell:start']);
 grunt.registerTask('files', ['sync:main']);
+grunt.registerTask('para', ['parallel']);
 grunt.registerTask('test', ['qunit', 'jshint'], function() {
   grunt.log.write('Testing and hinting...');   
+});
+
+grunt.registerTask('alldone', function() {
+  grunt.log.writeln('Upload folder app running at http://localhost:1030');
 });
   
 
