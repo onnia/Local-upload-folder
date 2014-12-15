9e90ad49650011ade78e7d425b781164cf654fdb
onnia <kualtonen@gmail.com>
2014-12-15 20:00:07 +0200

    Grunt coveralls task now works

--- gruntfile.js
+++ gruntfile.js
@@ -39,16 +39,8 @@ module.exports = function (grunt) {
         /* code coveridge copied from docs*/
         coveralls: {
             options: {
-                // LCOV coverage file relevant to every target
                 src: 'instrument/reports/lcov.info',
-                // When true, grunt-coveralls will only print a warning rather than
-                // an error, to prevent CI builds from failing unnecessarily (e.g. if
-                // coveralls.io is down). Optional, defaults to false.
                 force: false
-            },
-            your_target: {
-                // Target-specific LCOV coverage file
-                src: 'instrument/reports/lcov.info'
             }
         },
         cover: {
