f1379fdf8ee3f8d14f7eb75e3561bd3a37cbace3
Onni Aaltonen <kualtonen@gmail.com>
2014-12-18 09:58:11 +0200

    Adding test examples

--- tests/index_spec.js
+++ tests/index_spec.js
@@ -1,37 +1,36 @@
 var index = require('../routes/index');
 
 /* ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+ https://github.com/caolan/nodeunit
+ 
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
 module.exports = {
-    testIndex: function(test) {
-    
-    test.expect(1);
-   
-index['/'].fn({}, {
-  render: function(data, json) {
-   console.log(data);
-   console.log(json);
-      test.deepEqual(json,{ title: 'upload-folder' });
-  }
-});
+    testIndex: function (test) {
+        test.expect(1);
 
-test.done();
-    
-}
+        index['/'].fn({}, {
+            render: function (data, json) {
+                console.log(data);
+                console.log(json);
+                test.deepEqual(json, {title: 'upload-folder'});
+            }
+        });
+
+        test.done();
+
+    }
 };
--- tests/server_spec.js
+++ tests/server_spec.js
@@ -1,13 +1,47 @@
-var server = re
+var server = require('../lib/server');
+var sinon = require('sinon');
 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var serveIndex = require('serve-index');
+var debug = require('debug')('app');
+var routes = require('../routes/index');
+var router = express.Router();
 
-var sinon = reegb
-
+/* ======== A Handy Little Nodeunit Reference ========
+ https://github.com/caolan/nodeunit
+ 
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
 
 exports.server = function(test){
     test.expect(1);
-    var spy = sinon.createSpy();
-    var s = server.init
+    var spy = sinon.spy();
+    var proxy = server.start(spy);
+    var s = server.initialise;
+    
+    PubSub.subscribe("message", spy );
+    
+    proxy();
+    
+    assert(spy.called);
     
     test.ok(s);
     test.done();
